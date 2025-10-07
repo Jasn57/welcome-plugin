fe59acdf220cc1623352f5dcf873f7f4e1a5d633
Jason <jasn74939@gmail.com>
2025-10-07 17:40:26 -0400

    add code comments

--- src/main/me/jason/welcome/Welcome.java
+++ src/main/me/jason/welcome/Welcome.java
@@ -1,5 +1,6 @@
 package main.me.jason.welcome;
 
+// import from game
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -7,17 +8,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Welcome extends JavaPlugin implements Listener {
 
+// when on    
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
         getLogger().info("welcomePlugin enabled");
     }
 
+// when off    
     @Override
     public void onDisable() {
         getLogger().info("welcomePlugin disabled");
     }
 
+// when player joins send a message    
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         event.getPlayer().sendMessage("§aWelcome to the server, §e" + event.getPlayer().getName() + "§a");
